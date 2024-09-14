0a3067e3aac903c6559aeac4fb93cadf42d522b3
Kuvarti <uzali.kafasi@gmail.com>
2024-09-14 16:58:53 +0300

    change main

--- project/main.cpp
+++ project/main.cpp
@@ -1,99 +1,109 @@
 #define SDL_MAIN_HANDLED
-#include <SDL.h>
-#include <SDL_vulkan.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
 #include <vulkan/vulkan.h>
 #include <iostream>
 #include <vector>
 
-int main(int argc, char* argv[]) {
-    // SDL başlatılıyor
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL başlatılamadı: " << SDL_GetError() << std::endl;
-        return -1;
-    }
+int main(int argc, char *argv[])
+{
+	// SDL başlatılıyor
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cerr << "SDL başlatılamadı: " << SDL_GetError() << std::endl;
+		return -1;
+	}
 
-    // SDL penceresi oluşturuluyor
-    SDL_Window* window = SDL_CreateWindow("SDL2 ve Vulkan Test",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          800, 600,
-                                          SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
-    if (!window) {
-        std::cerr << "Pencere oluşturulamadı: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return -1;
-    }
+	// SDL penceresi oluşturuluyor
+	SDL_Window *window = SDL_CreateWindow("SDL2 ve Vulkan Test",
+										  SDL_WINDOWPOS_CENTERED,
+										  SDL_WINDOWPOS_CENTERED,
+										  800, 600,
+										  SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
+	if (!window)
+	{
+		std::cerr << "Pencere oluşturulamadı: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return -1;
+	}
 
-    // Vulkan instance oluşturuluyor
-    VkInstance instance;
-    VkApplicationInfo appInfo{};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "SDL2 Vulkan Test";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+	// Vulkan instance oluşturuluyor
+	VkInstance instance;
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "SDL2 Vulkan Test";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
 
-    // SDL'in Vulkan ile kullanacağı gerekli uzantılar alınıyor
-    unsigned int extensionCount = 0;
-    if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr)) {
-        std::cerr << "SDL Vulkan uzantıları alınamadı: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+	// SDL'in Vulkan ile kullanacağı gerekli uzantılar alınıyor
+	unsigned int extensionCount = 0;
+	if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr))
+	{
+		std::cerr << "SDL Vulkan uzantıları alınamadı: " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
 
-    std::vector<const char*> extensions(extensionCount);
-    if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensions.data())) {
-        std::cerr << "SDL Vulkan uzantıları alınamadı (veri ile): " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+	std::vector<const char *> extensions(extensionCount);
+	if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensions.data()))
+	{
+		std::cerr << "SDL Vulkan uzantıları alınamadı (veri ile): " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
 
-    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-    createInfo.ppEnabledExtensionNames = extensions.data();
+	createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	createInfo.ppEnabledExtensionNames = extensions.data();
 
-    // Vulkan instance oluşturuluyor
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-        std::cerr << "Vulkan instance oluşturulamadı!" << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+	// Vulkan instance oluşturuluyor
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+	{
+		std::cerr << "Vulkan instance oluşturulamadı!" << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
 
-    // Pencerenin Vulkan surface'i oluşturuluyor
-    VkSurfaceKHR surface;
-    if (!SDL_Vulkan_CreateSurface(window, instance, &surface)) {
-        std::cerr << "Vulkan surface oluşturulamadı: " << SDL_GetError() << std::endl;
-        vkDestroyInstance(instance, nullptr);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+	// Pencerenin Vulkan surface'i oluşturuluyor
+	VkSurfaceKHR surface;
+	if (!SDL_Vulkan_CreateSurface(window, instance, &surface))
+	{
+		std::cerr << "Vulkan surface oluşturulamadı: " << SDL_GetError() << std::endl;
+		vkDestroyInstance(instance, nullptr);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
 
-    std::cout << "SDL2 ve Vulkan başarılı bir şekilde çalışıyor." << std::endl;
+	std::cout << "SDL2 ve Vulkan başarılı bir şekilde çalışıyor." << std::endl;
 
-    // SDL penceresi açıkken basit bir döngü
-    bool isRunning = true;
-    SDL_Event event;
-    while (isRunning) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                isRunning = false;
-            }
-        }
-    }
+	// SDL penceresi açıkken basit bir döngü
+	bool isRunning = true;
+	SDL_Event event;
+	while (isRunning)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				isRunning = false;
+			}
+		}
+	}
 
-    // Temizleme işlemleri
-    vkDestroySurfaceKHR(instance, surface, nullptr);
-    vkDestroyInstance(instance, nullptr);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	// Temizleme işlemleri
+	vkDestroySurfaceKHR(instance, surface, nullptr);
+	vkDestroyInstance(instance, nullptr);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
-    return 0;
+	return 0;
 }

4a30704969955f4b99f640cd37550be91e09389f
Fatih Çil <fatihcil.2001@gmail.com>
2024-09-26 19:08:20 +0300

    Add mouse controls and movement

--- project/inc/KeyboardHandler.hpp
+++ project/inc/KeyboardHandler.hpp
@@ -5,13 +5,20 @@
 
 class KeyboardHandler {
 public:
-    KeyboardHandler() : rotation(0.0f) {}
+    KeyboardHandler() : rotation(0.0f), position(0.0f), lastMousePosition(0.0f) {}
     void processInput();
+    void handleMouseMotion(int xrel, int yrel);
 
     glm::vec3 getRotation() const {
         return rotation;
     }
 
+    glm::vec3 getPosition() const {
+        return position;
+    }
+
 private:
     glm::vec3 rotation;
+    glm::vec3 position;
+    glm::vec2 lastMousePosition;
 };

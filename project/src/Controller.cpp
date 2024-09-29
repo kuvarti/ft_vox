469331c92eafba7376a4764a6502b8c10c835525
Fatih Çil <fatihcil.2001@gmail.com>
2024-09-29 16:58:50 +0300

    Update movement and rotation speeds in Controller class

--- project/src/Controller.cpp
+++ project/src/Controller.cpp
@@ -3,27 +3,51 @@
 void Controller::processInput() {
     const Uint8* state = SDL_GetKeyboardState(NULL);
 
-    if (state[SDL_SCANCODE_A]) {
-        position.x -= movementSpeed;
-    }
-    if (state[SDL_SCANCODE_D]) {
-        position.x += movementSpeed;
-    }
+    glm::vec3 forward = getForwardVector();
+    glm::vec3 right = getRightVector();
+
     if (state[SDL_SCANCODE_W]) {
-        position.y -= movementSpeed;
+        position += forward * movementSpeed; // Move forward
     }
     if (state[SDL_SCANCODE_S]) {
-        position.y += movementSpeed;
+        position -= forward * movementSpeed; // Move backward
+    }
+    if (state[SDL_SCANCODE_A]) {
+        position -= right * movementSpeed; // Move left
+    }
+    if (state[SDL_SCANCODE_D]) {
+        position += right * movementSpeed; // Move right
     }
     if (state[SDL_SCANCODE_Q]) {
-        position.z -= movementSpeed;
+        position.y -= movementSpeed; // Move down
     }
     if (state[SDL_SCANCODE_E]) {
-        position.z += movementSpeed;
+        position.y += movementSpeed; // Move up
+    }
+
+    if (state[SDL_SCANCODE_LEFT]) {
+        rotation.y -= rotationSpeed;
+    }
+    if (state[SDL_SCANCODE_RIGHT]) {
+        rotation.y += rotationSpeed;
     }
 }
 
 void Controller::handleMouseMotion(int xrel, int yrel) {
     // rotation.y += xrel * rotationSpeed;
     // rotation.x -= yrel * rotationSpeed;
+}
+
+glm::vec3 Controller::getForwardVector() const {
+    glm::vec3 forward;
+    forward.x = cos(glm::radians(rotation.y)) * cos(glm::radians(rotation.x));
+    forward.y = sin(glm::radians(rotation.x));
+    forward.z = sin(glm::radians(rotation.y)) * cos(glm::radians(rotation.x));
+    return glm::normalize(forward);
+}
+
+glm::vec3 Controller::getRightVector() const {
+    glm::vec3 forward = getForwardVector();
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    return glm::normalize(glm::cross(forward, up));
 }

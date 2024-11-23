1346ea76e46c835e3835e078f85512df98208379
Kuvarti <uzali.kafasi@gmail.com>
2024-11-23 07:06:35 +0300

    New Env for vertex positions

--- project/inc/Settings.hpp
+++ project/inc/Settings.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 struct Settings{
-	Settings(int grid, int chunk, int calc, int seed, int maxflight, int width, int height) :
-	GridSize(grid), ChunkSize(chunk), CalcRange(calc), Seed(seed), MaxFrameInFlight(maxflight), ScreenWidth(width), ScreenHeight(height)
+	Settings(int grid, int chunk, int calc, int seed, int maxflight, int width, int height, float c_size) :
+	GridSize(grid), ChunkSize(chunk), CalcRange(calc), Seed(seed), MaxFrameInFlight(maxflight), ScreenWidth(width), ScreenHeight(height), cubeSize(c_size), halfCubeSize(c_size / 2)
 	{};
-	Settings(): GridSize(0), ChunkSize(0), CalcRange(0), Seed(0), MaxFrameInFlight(0), ScreenWidth(0), ScreenHeight(0) {};
+	Settings(): GridSize(0), ChunkSize(0), CalcRange(0), Seed(0), MaxFrameInFlight(0), ScreenWidth(0), ScreenHeight(0), cubeSize(0), halfCubeSize(0) {};
 	int GridSize;
 	int ChunkSize;
 	int CalcRange;
@@ -12,6 +12,8 @@ struct Settings{
 	int MaxFrameInFlight;
 	unsigned int ScreenWidth;
 	unsigned int ScreenHeight;
+	float cubeSize;
+	float halfCubeSize;
 };
 
 class Env{
--- project/src/Settings.cpp
+++ project/src/Settings.cpp
@@ -17,7 +17,7 @@ int InitSettings(char *arg)
 	std::ifstream settingsFile(file);
 	if (!settingsFile.is_open()) {
 		std::cout << "Error: Could not open settings file or doesnt find default settings file.\n\tProgram will continue with default settings." << std::endl;
-		env = new Env(Settings(128, 16, 10, rand(), 2, 800, 600));
+		env = new Env(Settings(128, 16, 10, rand(), 2, 800, 600, 1.0f));
 		return 2;
 	}
 	std::unordered_map<std::string, int> settingsMap;
@@ -39,7 +39,8 @@ int InitSettings(char *arg)
 		settingsMap.find("SEED") != settingsMap.end() ? settingsMap["SEED"] == 0 ? rand() : settingsMap["SEED"] : rand(),
 		settingsMap.find("MAX_FRAMES_IN_FLIGHT") != settingsMap.end() ? settingsMap["MAX_FRAMES_IN_FLIGHT"] : 2,
 		settingsMap.find("WIDTH") != settingsMap.end() ? settingsMap["WIDTH"] : 800,
-		settingsMap.find("HEIGHT") != settingsMap.end() ? settingsMap["HEIGHT"] : 600
+		settingsMap.find("HEIGHT") != settingsMap.end() ? settingsMap["HEIGHT"] : 600,
+		settingsMap.find("CUBE_SIZE") != settingsMap.end() ? settingsMap["CUBE_SIZE"] : 1.0f
 	));
 	settingsFile.close();
 	if (env == NULL)
